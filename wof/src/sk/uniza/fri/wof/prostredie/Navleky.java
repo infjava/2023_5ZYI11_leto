3003eae7cc3c91140cd5536620e694ba91fb5630
Jan Janech <jan.janech@fri.uniza.sk>
2024-02-27 09:40:08 +0100

    02 - navleky su samostatna implementacia interface Predmet

---
+++ wof/src/sk/uniza/fri/wof/prostredie/Navleky.java
@@ -0,0 +1,26 @@
+package sk.uniza.fri.wof.prostredie;
+
+public class Navleky implements Predmet {
+    private boolean obute;
+
+    public Navleky() {
+        this.obute = false;
+    }
+
+    @Override
+    public String getNazov() {
+        return "navleky";
+    }
+
+    @Override
+    public void pouzi() {
+        this.obute = !this.obute;
+
+        // System.out.println(this.obute ? "Obul si si navleky" : "Vyzul si si navleky");
+        if (this.obute) {
+            System.out.println("Obul si si navleky");
+        } else {
+            System.out.println("Vyzul si si navleky");
+        }
+    }
+}
--- wof/src/sk/uniza/fri/wof/prostredie/ObycajnyPredmet.java
+++ wof/src/sk/uniza/fri/wof/prostredie/ObycajnyPredmet.java
@@ -5,11 +5,9 @@ import java.time.format.DateTimeFormatter;
 
 public class ObycajnyPredmet implements Predmet {
     private final String nazov;
-    private boolean obute;
 
     public ObycajnyPredmet(String nazov) {
         this.nazov = nazov;
-        this.obute = false;
     }
 
     @Override
@@ -25,16 +23,6 @@ public class ObycajnyPredmet implements Predmet {
                 var now = LocalDateTime.now();
 
                 System.out.println(dtf.format(now));
-                break;
-            case "navleky":
-                this.obute = !this.obute;
-
-                // System.out.println(this.obute ? "Obul si si navleky" : "Vyzul si si navleky");
-                if (this.obute) {
-                    System.out.println("Obul si si navleky");
-                } else {
-                    System.out.println("Vyzul si si navleky");
-                }
                 break;
             default:
                 System.out.format("Predmet %s sa neda pouzit%n", this.nazov);

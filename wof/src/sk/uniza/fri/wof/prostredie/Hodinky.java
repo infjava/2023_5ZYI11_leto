015d7b884589e69673681d6a5ab7df4a02dcb568
Jan Janech <jan.janech@fri.uniza.sk>
2024-02-27 09:43:44 +0100

    02 - hodinky su samostatna implementacia interface Predmet

---
+++ wof/src/sk/uniza/fri/wof/prostredie/Hodinky.java
@@ -0,0 +1,19 @@
+package sk.uniza.fri.wof.prostredie;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Hodinky implements Predmet {
+    @Override
+    public String getNazov() {
+        return "hodinky";
+    }
+
+    @Override
+    public void pouzi() {
+        var dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+        var now = LocalDateTime.now();
+
+        System.out.println(dtf.format(now));
+    }
+}
--- wof/src/sk/uniza/fri/wof/prostredie/ObycajnyPredmet.java
+++ wof/src/sk/uniza/fri/wof/prostredie/ObycajnyPredmet.java
@@ -1,8 +1,5 @@
 package sk.uniza.fri.wof.prostredie;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class ObycajnyPredmet implements Predmet {
     private final String nazov;
 
@@ -17,15 +14,6 @@ public class ObycajnyPredmet implements Predmet {
 
     @Override
     public void pouzi() {
-        switch (this.nazov) {
-            case "hodinky":
-                var dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
-                var now = LocalDateTime.now();
-
-                System.out.println(dtf.format(now));
-                break;
-            default:
-                System.out.format("Predmet %s sa neda pouzit%n", this.nazov);
-        }
+        System.out.format("Predmet %s sa neda pouzit%n", this.nazov);
     }
 }

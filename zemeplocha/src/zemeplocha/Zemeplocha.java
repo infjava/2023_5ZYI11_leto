b8f8f6721b67b524bc030de69dd20b061df69a97
Jan Janech <jan.janech@fri.uniza.sk>
2024-03-26 08:11:46 +0100

    04 - riesenie lovu pomocou novej triedy Zver

--- zemeplocha/src/zemeplocha/Zemeplocha.java
+++ zemeplocha/src/zemeplocha/Zemeplocha.java
@@ -1,10 +1,7 @@
 package zemeplocha;
 
 import hlavnyBalik.Policko;
-import obyvatelia.Lovci;
-import obyvatelia.Magovia;
-import obyvatelia.Tvory;
-import obyvatelia.TypObyvatela;
+import obyvatelia.*;
 
 public class Zemeplocha {
     //pocet poludnikov aj rovnobeziek je rovnaky
@@ -29,7 +26,7 @@ public class Zemeplocha {
             for (int j = 0; j < this.policka[i].length; j++) {
 
                 if ((cislo % 2) == 0) { //vela zveri
-                    this.policka[i][j].setObyvatelia(new Tvory (25, TypObyvatela.SRNKY));
+                    this.policka[i][j].setObyvatelia(new Zver(25, TypObyvatela.SRNKY));
                 } else {
                     if ((cislo % 13) == 0) {
                         this.policka[i][j].setObyvatelia(new Magovia(5, 3));
@@ -37,9 +34,9 @@ public class Zemeplocha {
                         this.policka[i][j].setObyvatelia(new Lovci(20));
                     } else if ((cislo % 5) == 0) {
                         if ((i % 2) == 0) { //sem tam ine
-                            this.policka[i][j].setObyvatelia(new Tvory(10, TypObyvatela.ZAJACE));
+                            this.policka[i][j].setObyvatelia(new Zver(10, TypObyvatela.ZAJACE));
                         } else {
-                            this.policka[i][j].setObyvatelia(new Tvory(10, TypObyvatela.SLIEPKY));
+                            this.policka[i][j].setObyvatelia(new Zver(10, TypObyvatela.SLIEPKY));
                         }
                     }
                 }

29187dabe832cd3e27980d047566ab90b3f6f919
Jan Janech <jan.janech@fri.uniza.sk>
2024-03-26 09:25:41 +0100

    06 - riesenie duplicit medzi delenim a scitanim pomocou polymorfizmu

---
+++ vynimky_matica_basic/src/sk/uniza/fri/operacie/Operacia.java
@@ -0,0 +1,5 @@
+package sk.uniza.fri.operacie;
+
+public interface Operacia {
+    double vypocitaj(double operand1, double operand2);
+}
---
+++ vynimky_matica_basic/src/sk/uniza/fri/operacie/Scitanie.java
@@ -0,0 +1,8 @@
+package sk.uniza.fri.operacie;
+
+public class Scitanie implements Operacia {
+    @Override
+    public double vypocitaj(double operand1, double operand2) {
+        return operand1 + operand2;
+    }
+}

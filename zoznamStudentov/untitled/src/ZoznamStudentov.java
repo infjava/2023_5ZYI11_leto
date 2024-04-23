646a9321c8d35330c7cfbd33aa19e822bd86095f
Jan Janech <jan.janech@fri.uniza.sk>
2024-04-23 10:14:48 +0200

    09 - zoznam studentov sa zobrazuje v hlavnom okne

--- zoznamStudentov/untitled/src/ZoznamStudentov.java
+++ zoznamStudentov/untitled/src/ZoznamStudentov.java
@@ -2,13 +2,22 @@ import javax.swing.*;
 
 public class ZoznamStudentov {
     private final JFrame okno;
+    private final DefaultListModel<Student> studenti;
     private JPanel obsahOkna;
-    private JList zoznamStudentov;
+    private JList<Student> zoznamStudentov;
     private JButton pridaj;
     private JButton vymaz;
     private JButton uprav;
 
     public ZoznamStudentov() {
+        this.studenti = new DefaultListModel<Student>();
+
+        this.studenti.addElement(new Student("Jozko", "Mrkvicka"));
+        this.studenti.addElement(new Student("Ferko", "Mrkvicka"));
+        this.studenti.addElement(new Student("Martin", "Mrkvicka"));
+
+        this.zoznamStudentov.setModel(this.studenti);
+
         this.okno = new JFrame("Zoznam študentov");
         this.okno.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.okno.add(this.obsahOkna);

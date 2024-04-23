f1f8cdbc5ec6d525b53e149a0841496f0e5ae3cd
Jan Janech <jan.janech@fri.uniza.sk>
2024-04-23 08:33:17 +0200

    09 - prva verzia okna aplikacie

--- kravina/src/Main.java
+++ kravina/src/Main.java
@@ -1,4 +1,9 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        var okno = new JFrame("Otazka");
+        okno.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        okno.setVisible(true);
     }
 }
